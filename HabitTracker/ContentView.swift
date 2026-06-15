9a40b77f1a4e2cd0602c19bedf948a006399daae
Jacob Andrean <jacob.andrean@icloud.com>
2026-06-15 07:53:50 +0700

    initial commit for xcode cloud
    initial commit for xcode cloud
    
    initial commit for xcode cloud

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -19,25 +19,10 @@ struct HabitTrackerApp: App {
 /// side-by-side in the same app, using the same shared persistence layer.
 struct ArchitectureSelectorView: View {
     @Environment(\.modelContext) private var modelContext
-    @State private var selectedTab = ArchitectureTab.mv
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Tab("MV", systemImage: "m.circle.fill", value: ArchitectureTab.mv) {
-                MVStack(modelContext: modelContext)
-            }
-            Tab("MVVM", systemImage: "v.circle.fill", value: ArchitectureTab.mvvm) {
-                MVVMStack(modelContext: modelContext)
-            }
-            Tab("TCA", systemImage: "t.circle.fill", value: ArchitectureTab.tca) {
-                TCAStack(modelContext: modelContext)
-            }
-        }
+        MVStack(modelContext: modelContext)
     }
-}
-
-enum ArchitectureTab: Equatable {
-    case mv, mvvm, tca
 }
 
 // MARK: - Architecture Stacks
@@ -49,20 +34,6 @@ struct MVStack: View {
     let modelContext: ModelContext
     var body: some View {
         MVRootView(repository: SwiftDataHabitRepository(modelContext: modelContext))
-    }
-}
-
-struct MVVMStack: View {
-    let modelContext: ModelContext
-    var body: some View {
-        MVVMRootView(repository: SwiftDataHabitRepository(modelContext: modelContext))
-    }
-}
-
-struct TCAStack: View {
-    let modelContext: ModelContext
-    var body: some View {
-        TCAAppView(repository: SwiftDataHabitRepository(modelContext: modelContext))
     }
 }
 
