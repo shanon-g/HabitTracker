ca9fad5f4c3efae2a9e6165a844d02d5bef3a8ac
Jacob Andrean <jacob.andrean@icloud.com>
2026-07-06 17:27:29 +0700

    added intentional bug

--- HabitTracker/Domain/UseCases/CreateHabitUseCase.swift
+++ HabitTracker/Domain/UseCases/CreateHabitUseCase.swift
@@ -19,7 +19,7 @@ struct CreateHabitUseCase: Sendable {
         description: String = "",
         frequency: HabitFrequency = .daily
     ) async throws -> Habit {
-        let trimmed = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        let trimmed = name.trimmingCharacters(in: .newlines)
         guard !trimmed.isEmpty else {
             throw HabitError.emptyName
         }
